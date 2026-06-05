5deac14a88a05edbee4d21873b61dfa0ae37a94a
Axel-Jeremy <axeljeremy2802@gmail.com>
2026-06-05 22:12:07 +0700

    add calculate fitness value

--- src/main/java/FSS.java
+++ src/main/java/FSS.java
@@ -11,11 +11,26 @@ public class FSS {
         this.machineCount = machineCount;
     }
 
-    public FitnessValue calculateFitnessFunction(List<Integer> jobSchedule) {
+    public boolean isFitnessBetter(FitnessValue current, FitnessValue prev) {
+        int prevMakespan = prev.makespan();
+        int currMakespan = current.makespan();
+
+        int prevTotalFlowTime = prev.totalFlowTime();
+        int currTotalFlowTime = current.totalFlowTime();
+        
+        double minMakespan = Math.min(currMakespan, prevMakespan);
+        double minTotalFlowTime = Math.min(currTotalFlowTime, prevTotalFlowTime);
+
+        double prevFitness = (prevMakespan - minMakespan) / minMakespan + (prevTotalFlowTime - minTotalFlowTime) / minTotalFlowTime;
+        double currentFitness = (currMakespan - minMakespan) / minMakespan + (currTotalFlowTime - minTotalFlowTime) / minTotalFlowTime;
+        return Double.compare(currentFitness, prevFitness) < 0;
+    }
+
+    public FitnessValue calculateFitness(List<Integer> jobSchedule) {
         int[][] completionTime = new int[jobCount][machineCount];
         completionTime[0][0] = processingTime[jobSchedule.getFirst()][0];
         for (int i = 1; i < machineCount; i++) {
-            completionTime[0][i] = completionTime[0][i - 1] + processingTime[jobSchedule.get(0)][i];
+            completionTime[0][i] = completionTime[0][i - 1] + processingTime[jobSchedule.getFirst()][i];
         }
 
         for (int i = 1; i < jobCount; i++) {
@@ -26,12 +41,16 @@ public class FSS {
             }
         }
 
-        int makespan = completionTime[jobCount - 1][machineCount - 1];
+        int makespan = calculateMakespan(completionTime);
         int totalFlowTime = calculateTotalFlowTime(completionTime);
         return new FitnessValue(makespan, totalFlowTime);
     }
+    
+    private int calculateMakespan(int[][] completionTime) {
+        return completionTime[jobCount - 1][machineCount - 1];
+    }
 
-    public int calculateTotalFlowTime(int[][] completionTime) {
+    private int calculateTotalFlowTime(int[][] completionTime) {
         int totalFlowTime = 0;
         for (int i = 0; i < jobCount; i++) {
             totalFlowTime += completionTime[i][machineCount - 1];
