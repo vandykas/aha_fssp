454a7d17ebcc35769322a03f49a4ad20a1995ea3
Vandyka Suryadi <vandykasuryadi@gmail.com>
2026-06-24 17:49:53 +0700

    Menambahkan kode untuk eksperimen langsung semuanya

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,59 +10,65 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        for (int fileNum = 0; fileNum < 10; fileNum++) {
-            File dataFile = new File(String.format("src/main/resources/ins_20_10_0%d.txt", fileNum));
+        String[] jobSize = {"10", "100", "200"};
+        String[] machineSize = {"10", "20"};
+        for (String js : jobSize) {
+            for (String ms : machineSize) {
+                for (int fileNum = 0; fileNum < 10; fileNum++) {
+                    File dataFile = new File(String.format("src/main/resources/ins_%s_%s_0%d.txt", js, ms, fileNum));
 
-            int jobCount = 0; 
-            int machineCount = 0;
-            int[][] processingTime = null;
+                    int jobCount = 0; 
+                    int machineCount = 0;
+                    int[][] processingTime = null;
 
-            if (dataFile.exists()) {
-                try (BufferedReader bf = new BufferedReader(new FileReader(dataFile))) {
-                    String line = bf.readLine();
-                    String[] input = line.trim().split("\\s+");
-                    jobCount =  Integer.parseInt(input[0]);
-                    machineCount =  Integer.parseInt(input[1]);
-                    processingTime = new int[jobCount][machineCount];
+                    if (dataFile.exists()) {
+                        try (BufferedReader bf = new BufferedReader(new FileReader(dataFile))) {
+                            String line = bf.readLine();
+                            String[] input = line.trim().split("\\s+");
+                            jobCount =  Integer.parseInt(input[0]);
+                            machineCount =  Integer.parseInt(input[1]);
+                            processingTime = new int[jobCount][machineCount];
 
-                    int i = 0;
-                    while ((line = bf.readLine()) != null) {
-                        input = line.trim().split("\\s+");
-                        for (int j = 0; j < jobCount; j++) {
-                            processingTime[j][i] = Integer.parseInt(input[j]); 
+                            int i = 0;
+                            while ((line = bf.readLine()) != null) {
+                                input = line.trim().split("\\s+");
+                                for (int j = 0; j < jobCount; j++) {
+                                    processingTime[j][i] = Integer.parseInt(input[j]); 
+                                }
+                                i++;
+                            }
                         }
-                        i++;
+                        catch (IOException e) {
+                            System.out.println("Error " + e.getMessage());
+                        }
                     }
-                }
-                catch (IOException e) {
-                    System.out.println("Error " + e.getMessage());
+
+                    FSS fss = new FSS(processingTime, jobCount, machineCount);
+                    Random rand = new Random(42);
+                    File outputFile = new File(String.format("src/main/resources/result_%s_%s_0%d.csv", js, ms, fileNum));
+                    try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
+                        bw.write("population_size,makespan,total_flow_time\n");
+                        for (int i = 10; i <= 20; i+=10) {
+                            AHA aha = new AHA(fss, rand, i, 500, jobCount);
+                            Hummingbird best = aha.run();
+
+                            ArrayList<Integer> sol = best.getFoodSource().getJobSchedule();
+                            bw.write(i + "," + best.getFoodSource().getMakespan() + "," + best.getFoodSource().getTotalFlowTime() + "\n");
+
+                            // System.out.print("[" + (sol.get(0) + 1));
+                            // for (int i = 1; i < sol.size(); i++) {
+                            //     System.out.print(", " + (sol.get(i) + 1));
+                            // }
+                            // System.out.print("]\n");
+                            // System.out.println("Makespan: " + best.getFoodSource().getMakespan());
+                            // System.out.println("Tft: " + best.getFoodSource().getTotalFlowTime());
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
                 }
             }
-
-            FSS fss = new FSS(processingTime, jobCount, machineCount);
-            Random rand = new Random(42);
-            File outputFile = new File(String.format("src/main/resources/result_20_10_0%d.csv", fileNum));
-            try (BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile))) {
-                bw.write("population_size,makespan,total_flow_time\n");
-                for (int i = 10; i <= 20; i+=10) {
-                    AHA aha = new AHA(fss, rand, i, 1000, jobCount);
-                    Hummingbird best = aha.run();
-
-                    ArrayList<Integer> sol = best.getFoodSource().getJobSchedule();
-                    bw.write(i + "," + best.getFoodSource().getMakespan() + "," + best.getFoodSource().getTotalFlowTime() + "\n");
-
-                    // System.out.print("[" + (sol.get(0) + 1));
-                    // for (int i = 1; i < sol.size(); i++) {
-                    //     System.out.print(", " + (sol.get(i) + 1));
-                    // }
-                    // System.out.print("]\n");
-                    // System.out.println("Makespan: " + best.getFoodSource().getMakespan());
-                    // System.out.println("Tft: " + best.getFoodSource().getTotalFlowTime());
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
         }
     }
 }
